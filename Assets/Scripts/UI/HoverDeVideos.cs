e784bdf92b0129cde7d3ba8b70e6f673f2dae485
morita2232 <sebastian.mora@enti.cat>
2025-04-22 11:01:30 +0200

    Integracion de videos hecha

---
+++ Assets/Scripts/UI/HoverDeVideos.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class HoverDeVideos : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public GameObject hoverPanel;
+    public VideoPlayer player;
+    public Button curarDefault;
+
+    void Start()
+    {
+        hoverPanel.SetActive(false);
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(curarDefault.interactable == true)
+        {
+        hoverPanel.SetActive(true);
+        player.Play();
+        }
+
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(curarDefault.interactable == true)
+        {
+        hoverPanel.SetActive(false);
+        player.Stop();
+        }
+    }
+}
